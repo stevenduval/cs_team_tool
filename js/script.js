598ffb1a529c96b10b2ba299c754f13d5117ddf0
stevenduval <stevenduval@gmail.com>
2020-05-19 17:01:10 -0500

    added date to filename and changed file name

--- js/script.js
+++ js/script.js
@@ -41,7 +41,8 @@ const saveOutput = () => {
     const text = output.querySelector("textarea").value.replace(/\n/g, "\r\n"); // To retain the Line breaks.
     const blob = new Blob([text], { type: "text/plain"});
     const anchor = document.createElement("a");
-    anchor.download = "link_generator_output.txt";
+    const date = new Date().toJSON().slice(0,10).split('-').join('/');
+    anchor.download = `cs_team_tool_output_${date}.txt`;
     anchor.href = window.URL.createObjectURL(blob);
     anchor.target ="_blank";
     anchor.style.display = "none"; // just to be safe!
