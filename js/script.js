b1cc80bb228e6604f57654ea63e19b27ab5d4587
stevenduval <stevenduval@gmail.com>
2020-05-20 08:16:09 -0500

    added uppercase and lowercase functionality

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ const removeBtn =  document.querySelector('.remove');
 const removeTOCBtn =  document.querySelector('.remove_toc');
 const saveBtn =  document.querySelector('.save');
 const reloadBtn =  document.querySelector('.reload');
+const uppercaseBtn =  document.querySelector('.uppercase');
+const lowercaseBtn =  document.querySelector('.lowercase');
 const input = document.querySelector('#input');
 const output = document.querySelector('#output');
 const urlRegex = /(https?:\/\/[^\s]+)/g;
@@ -52,7 +54,7 @@ const saveOutput = () => {
     anchor.click();
     document.body.removeChild(anchor);
 }
-
+// run when remove toc is clicked
 const removeTOC = () => {
     const returnURL = (url) => {
        const newURL = decodeURIComponent(url.split('&click=')[1]);
@@ -60,12 +62,16 @@ const removeTOC = () => {
     }
     output.querySelector('textarea').value = input.querySelector('textarea').value.replace(/https:\/\/www.medtargetsystem.com\/toc.+?(?=")/g, returnURL);
 }
-
+// run when more than 1 character is typed in the box
 const characterCount = () => {
     const inputLength = input.querySelector('textarea').value.length;
     const characterCountSpan = input.querySelectorAll('span')[1];
     characterCountSpan.innerText = (inputLength > 0) ? `Character Count : ${inputLength}` : '';
 }
+
+const uppercase = () =>  output.querySelector('textarea').value = input.querySelector('textarea').value.toUpperCase();
+
+const lowercase = () => output.querySelector('textarea').value = input.querySelector('textarea').value.toLowerCase();
 
 //event listeners
 layout.addEventListener('change', changeLayout);
@@ -73,6 +79,8 @@ input.querySelector('textarea').addEventListener('keyup', characterCount);
 joinBtn.addEventListener('click', join);
 addBtn.addEventListener('click', addAnchors);
 removeBtn.addEventListener('click', removeAnchors);
+uppercaseBtn.addEventListener('click', uppercase);
+lowercaseBtn.addEventListener('click', lowercase);
 removeTOCBtn.addEventListener('click', removeTOC);
 saveBtn.addEventListener('click', saveOutput);
 reloadBtn.addEventListener('click', () => location.reload(true));
